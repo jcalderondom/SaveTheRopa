5dbad69ed35b3af2601619328db15abec4df0192
jcalderondom <juancamilo.calderondominguez@mercadolibre.com.co>
2024-02-06 15:57:26 -0500

    Creating repo
    Grupo 1 , ejercicio vivo 2 en 06 de febrero

---
+++ src/Dakar/Main.java
@@ -0,0 +1,14 @@
+package Dakar;
+
+public class Main {
+    public static void main(String[] args) {
+        Carrera carrera = new Carrera(1.5,30000,"La Carrera ",2);
+        Vehiculo moto = new Moto(3000,500,180,"LX58B");
+        Vehiculo carro = new Carro(3000,500,180,"LX58B");
+        carrera.darDeAltaVehiculo(moto);
+        carrera.darDeAltaVehiculo(carro);
+        carrera.imprimirLista();
+
+
+    }
+}
